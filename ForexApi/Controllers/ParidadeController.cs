38d0170ce81f46783d36009116dccd9ca14d93e9
Silvio Sebastiany <silvio.sebastiany.gmail.com>
2024-05-18 19:17:48 -0300

    002 - Add ParidadeController

---
+++ ForexApi/Controllers/ParidadeController.cs
@@ -0,0 +1,63 @@
+using ForexApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ForexApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ParidadesController : ControllerBase
+{
+    private readonly Contexto _contexto;
+
+    public ParidadesController(Contexto contexto)
+    {
+        _contexto = contexto;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Paridade>>> PegarTodosAsync()
+    {
+        return await _contexto.Paridades.ToListAsync();
+    }
+
+    [HttpGet("{paridadeId}")]
+    public async Task<ActionResult<Paridade>> PegarParidadePeloAsync(int paridadeId)
+    {
+        Paridade paridade = await _contexto.Paridades.FindAsync(paridadeId);
+
+        if(paridade == null)
+            return NotFound();
+        
+        return paridade;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Paridade>> SalvarParidadeAsync(Paridade paridade)
+    {
+        await _contexto.Paridades.AddAsync(paridade);
+        await _contexto.SaveChangesAsync();
+
+        return Ok();
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> AtualizarParidadeAsync(Paridade paridade)
+    {
+        _contexto.Paridades.Update(paridade);
+        await _contexto.SaveChangesAsync();
+        
+        return Ok();
+    }
+
+    [HttpDelete("{paridadeId}")]
+    public async Task<ActionResult> ExcluirParidadeAsync(int paridadeId)
+    {
+        Paridade paridade = await _contexto.Paridades.FindAsync(paridadeId);
+        _contexto.Remove(paridade);
+        await _contexto.SaveChangesAsync();
+
+        return Ok();
+    }
+
+}

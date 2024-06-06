609464eb1883b8ffb969b86d156c31a699c0ed28
Silvio Sebastiany <silvio.sebastiany.gmail.com>
2024-06-05 22:40:47 -0300

    Aula 06 -Formulario da Listagem dos dados

--- AppForexAngular/src/app/Components/paridades/paridades.component.ts
+++ AppForexAngular/src/app/Components/paridades/paridades.component.ts
@@ -13,7 +13,10 @@ export class ParidadesComponent implements OnInit{
 
   formulario:FormGroup;
   tituloFormulario:string = "";
+  paridades: Paridade[] = [];
 
+  visibilidadeTabela:boolean = true;
+  visibilidadeFormulario:boolean = false;
 
   constructor(private paridadeService : ParidadeService, private formBuilder: FormBuilder){
     this.formulario = this.formBuilder.group({
@@ -25,17 +28,39 @@ export class ParidadesComponent implements OnInit{
 
   ngOnInit(): void {
     this.tituloFormulario = 'Nova Paridade';
+
+    this.paridadeService.PegarTodos().subscribe(resultado => {
+      this.paridades = resultado;
+    });
+  }
+
+  ExibirFormularioCadastro(): void{
+    this.visibilidadeTabela = false;
+    this.visibilidadeFormulario = true;
+    this.tituloFormulario = 'Nova Paridade';
+    this.formulario = this.formBuilder.group({ // Use formBuilder.group para inicializar o FormGroup
+      moeda: [null, Validators.required],
+      preco: [null, Validators.required],
+      swap: [null, Validators.required]
+    });
   }
 
   EnviarFormulario(): void {
     if (this.formulario.valid) { 
       const paridade: Paridade = this.formulario.value;
       this.paridadeService.SalvarParidade(paridade).subscribe(resultado => {
+        this.visibilidadeTabela = true;
+        this.visibilidadeFormulario = false;
         alert('Paridade inserida com sucesso');
       });
     } else {
       alert('Por favor, preencha todos os campos obrigatórios.');
     }
+  }
+
+  Voltar():void{
+    this.visibilidadeTabela = true;
+    this.visibilidadeFormulario = false;
   }
 
 }
